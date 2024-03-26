40ba2a4b8c4a1b751411e36a91f475e3ca0757f5
daniel_flutur <daniel.flutur@assist.ro>
2024-03-26 11:22:47 +0200

    Implement internet conectivity check

--- Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
+++ Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MonkeyFinder.Services;
 using MonkeyFinder.View;
 
 namespace MonkeyFinder;
@@ -19,7 +20,18 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
+		builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+		builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+		builder.Services.AddSingleton<IMap>(Map.Default);
+
+		builder.Services.AddSingleton<MonkeyService>();
+
+		builder.Services.AddSingleton<MonkeysViewModel>();
+		builder.Services.AddTransient<MonkeyDetailsViewModel>();
+
 		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddTransient<DetailsPage>();
+
 
 		return builder.Build();
 	}
--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,78 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    IConnectivity connectivity;
+
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+    }
+
+    [RelayCommand]
+    async Task GoToDetailsAsync(Monkey monkey)
+    {
+        if (monkey == null)
+        {
+            return;
+        }
+
+        await Shell.Current.GoToAsync(nameof(DetailsPage), true,
+            new Dictionary<string, object>
+            {
+                {"Monkey", monkey }
+            });
+    }
+
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy) 
+            return;
+
+        try
+        {
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert(
+                    "Internet issue",
+                    $"Check your internet and try again!",
+                    "OK");
+
+                return;
+            }
+
+            IsBusy = true;
+
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if (Monkeys.Count != 0)
+            {
+                Monkeys.Clear();
+            }
+
+            foreach (var monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
+
+        }
+        catch ( Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get monkeys: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
